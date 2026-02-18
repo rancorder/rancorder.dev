7328d2153b0d0da270fca42168c3825440feaa55
hideki <xzengbu@gmail.com>
2026-02-18 15:18:52 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-  experimental: {
-    mdxRs: true,
-  },
-  // 外部画像許可（Qiita/Zennのサムネイル用）
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'qiita-user-contents.imgix.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 }
 
-const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
