4eb51300001b84694c93f5e6b6ecaa8f37e1b546
hideki <xzengbu@gmail.com>
2026-08-30 16:10:14 +0900

    Add canonical about page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -36,6 +36,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: 'weekly',
