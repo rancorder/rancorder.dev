a331cc1389e236e9e1aa0f01aa470f7f6d521d56
hideki <xzengbu@gmail.com>
2026-08-30 16:08:27 +0900

    Clarify AI crawler access for GEO

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,22 @@ export default function robots(): MetadataRoute.Robots {
         allow: '/',
         disallow: ['/api/', '/_next/', '/health-os'],
       },
+      {
+        userAgent: 'OAI-SearchBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'GPTBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'ClaudeBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'PerplexityBot',
+        allow: '/',
+      },
     ],
     host: 'https://rancorder.dev',
     sitemap: 'https://rancorder.dev/sitemap.xml',
